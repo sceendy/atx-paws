3e0690dd71a8a789274b28ff64568c5880c4a920
Cindy Juarez <sceendy@gmail.com>
2018-03-15 17:41:25 -0500

    initial setup

---
+++ app/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {styles} from './Typography';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>ATX Paws</h1>
+      </div>);
+  }
+}
---
+++ app/components/Button/README.md
@@ -0,0 +1,13 @@
+Basic button:
+
+```jsx
+<Button>Push Me</Button>
+```
+
+Big pink button:
+
+```jsx
+<Button size="large" color="deeppink">
+  Lick Me
+</Button>
+```
