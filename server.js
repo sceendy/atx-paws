dcfc7bafc82e53d7fae4a62f3edc59cd55a16413
Cindy Juarez <sceendy@gmail.com>
2018-05-03 18:08:37 -0500

    add port logic

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const PORT = process.env.PORT || 4000;
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('ATX Paw Finder running\n');
 });
