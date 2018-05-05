799aea06d80e15d2fb1672cb7104ccb372ac579a
Cindy Juarez <sceendy@gmail.com>
2018-05-05 13:30:37 -0500

    adding compression

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const compression = require('compression');
 
 const PORT = process.env.PORT || 4000;
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-app.get('*.js', (req, res, next) => {
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
 });
 
 app.listen(PORT, function () {
